8f3be139e4b0e157e48a014e9e2c6e9c4068734f
Kristian Bauer <kristian@kristianbauer.com>
2011-12-12 21:59:08 -0500

    Base cocos2d box2d project.

---
+++ BitBlast/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  BitBlast
+//
+//  Created by Kristian Bauer on 12/12/11.
+//  Copyright Bauerkraut 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ BitBlast/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  BitBlast
+//
+//  Created by Kristian Bauer on 12/12/11.
+//  Copyright Bauerkraut 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
