96dcac94a231a9d5eda820431513623a9116d285
Kristian Bauer <kristian@kristianbauer.com>
2011-12-16 16:30:34 -0500

    WIP camera and scrolling.

---
+++ BitBlast/CCFollowOffset.h
@@ -0,0 +1,16 @@
+//
+//  CCFollowOffset.h
+//  BitBlast
+//
+//  Created by Kristian Bauer on 12/16/11.
+//  Copyright 2011 Bauerkraut. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface CCFollowOffset : CCFollow {
+    
+}
+
+@end
---
+++ BitBlast/CCFollowOffset.m
@@ -0,0 +1,14 @@
+//
+//  CCFollowOffset.m
+//  BitBlast
+//
+//  Created by Kristian Bauer on 12/16/11.
+//  Copyright 2011 Bauerkraut. All rights reserved.
+//
+
+#import "CCFollowOffset.h"
+
+
+@implementation CCFollowOffset
+
+@end
