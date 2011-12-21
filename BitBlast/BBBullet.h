b4e323a16b5c0ce7e38ab35f70920decfd299796
Kristian Bauer <kristian@kristianbauer.com>
2011-12-21 08:59:32 -0500

    Fix for CCFollow to allow for scale. Camera now follows player's x movement with offset so they're on left of screen. Basic structure for shooting. Commented out for now. WIP camera following player's y movement if they hit certain boundaries.

---
+++ BitBlast/BBBullet.h
@@ -0,0 +1,16 @@
+//
+//  BBBullet.h
+//  BitBlast
+//
+//  Created by Kristian Bauer on 12/19/11.
+//  Copyright 2011 Bauerkraut. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface BBBullet : BBGameObject {
+    
+}
+
+@end
---
+++ BitBlast/BBBullet.m
@@ -0,0 +1,14 @@
+//
+//  BBBullet.m
+//  BitBlast
+//
+//  Created by Kristian Bauer on 12/19/11.
+//  Copyright 2011 Bauerkraut. All rights reserved.
+//
+
+#import "BBBullet.h"
+
+
+@implementation BBBullet
+
+@end
