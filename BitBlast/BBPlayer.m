7c5cccf31c6f95fb38f04ac3e7c5b7e42d60f83d
Bruce Morrison <bruce@ngmoco.com>
2012-02-10 23:35:27 -0800

    Pistol, Machine Gun, Shotgun, Gattlin Gun, Spread Gun, Blue Wave gun!

--- BitBlast/BBPlayer.m
+++ BitBlast/BBPlayer.m
@@ -71,7 +71,7 @@
 	weapons = [NSMutableArray new];
 	// create and add default weapon
 	BBWeapon *w = [BBWeapon new];
-	[w loadFromFile:@"testWeapon2"];
+	[w loadFromFile:@"bluewave"];
 	[w setEnabled:YES];
 	[weapons addObject:w];
 	[w release];
