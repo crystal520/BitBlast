bed34770bc63970feafee4fe2ca4419917af167c
Kristian Bauer <kristian@kristianbauer.com>
2012-02-16 20:48:44 -0500

    Default image format is now RGBA4444

--- BitBlast/AppDelegate.mm
+++ BitBlast/AppDelegate.mm
@@ -109,7 +109,7 @@
 	// Default texture format for PNG/BMP/TIFF/JPEG/GIF images
 	// It can be RGBA8888, RGBA4444, RGB5_A1, RGB565
 	// You can change anytime.
-	[CCTexture2D setDefaultAlphaPixelFormat:kCCTexture2DPixelFormat_RGBA8888];
+	[CCTexture2D setDefaultAlphaPixelFormat:kCCTexture2DPixelFormat_RGBA4444];
 	
 	// Removes the startup flicker
 	[self removeStartupFlicker];
