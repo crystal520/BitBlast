3a068de092938a17abc8b6931b003130e95ad613
Kristian Bauer <kristian@kristianbauer.com>
2012-07-23 16:33:09 -0400

    Jump down button added on iCade.

--- BitBlast/BBGameLayer.mm
+++ BitBlast/BBGameLayer.mm
@@ -511,16 +511,19 @@
 	else if(button == iCadeJoystickUp || button == iCadeJoystickUpLeft || button == iCadeJoystickUpRight) {
 		[player setWeaponAngle:1];
 	}
-	else if(button == iCadeButtonA || button == iCadeButtonB || button == iCadeButtonC || button == iCadeButtonD || button == iCadeButtonE || button == iCadeButtonF || button == iCadeButtonG || button == iCadeButtonH) {
+	else if(button == iCadeButtonA || button == iCadeButtonC || button == iCadeButtonE || button == iCadeButtonG) {
 		[player jump];
 	}
+    else if(button == iCadeButtonB || button == iCadeButtonD || button == iCadeButtonF || button == iCadeButtonH) {
+        [player jumpDown];
+    }
 }
 
 - (void) buttonUp:(iCadeState)button {
 	if(button == iCadeJoystickDown || button == iCadeJoystickDownLeft || button == iCadeJoystickDownRight || button == iCadeJoystickUp || button == iCadeJoystickUpLeft || button == iCadeJoystickUpRight) {
 		[player setWeaponAngle:0];
 	}
-	else if(button == iCadeButtonA || button == iCadeButtonB || button == iCadeButtonC || button == iCadeButtonD || button == iCadeButtonE || button == iCadeButtonF || button == iCadeButtonG || button == iCadeButtonH) {
+	else if(button == iCadeButtonA || button == iCadeButtonC || button == iCadeButtonE || button == iCadeButtonG) {
 		[player endJump];
 	}
 }
